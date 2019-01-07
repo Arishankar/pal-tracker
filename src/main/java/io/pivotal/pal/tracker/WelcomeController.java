a54dfc67c21863c2ffe830ece1f6f2251d3ab56f
Arishankar <arishankar@gmail.com>
2019-01-07 15:02:32 -0700

    Added for Cloudfoundry

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,24 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+  //  @Value("${welcome.message1}")
+    private String message;
+
+   public WelcomeController(@Value(value = "${welcome.message}") String message){
+       this.message = message;
+        
+    }
+
+
     @GetMapping("/")
     public String sayHello() {
-        return "Arishankar hello";
+
+        return message;
     }
 }
